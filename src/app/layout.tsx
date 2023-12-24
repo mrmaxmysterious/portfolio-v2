495b7591743b0cfaf850f78f7540f6a00ddcdf7c
Max <maxhenson28@icloud.com>
2023-12-24 10:53:34 +0000

    Auto December Snow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ export const metadata: Metadata = {
   title: 'Max Henson',
 }
 
+isChristmas = false
+const month = new Date().getMonth()
+if (month == 12) isChristmas = true
+
 export default function RootLayout({
   children,
 }: {
@@ -16,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="https://app.embed.im/snow.js" defer></script>
+        {isChristmas ? <script src="https://app.embed.im/snow.js" defer></script> : <p></p>
       </head>
       <body className={inter.className + " dark"}><div className="mx-auto max-w-3xl px-5">{children}</div></body>
     </html>
